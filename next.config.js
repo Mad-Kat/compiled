40ff19e10e56a887eff532d98d41776958ac0f41
Luca Schneider <luca.schneider@digitecgalaxus.ch>
2023-01-09 15:20:58 +0100

    Compiled works with next and extracts a single css file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
+const { CompiledExtractPlugin } = require('@compiled/webpack-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  distDir: '_next',
   webpack: (config) => {
+    config.module.generator.asset.publicPath = "/_next/";
     config.module.rules.push({
       test: /\.js$/,
-      use: ['@compiled/webpack-loader'],
+      exclude: /node_modules/,
+      use: [{
+        loader: '@compiled/webpack-loader',
+        options: {
+          extract: true,
+        }
+      }],
+    },{
+    test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
     })
+
+    config.plugins.push(new MiniCssExtractPlugin(
+      {
+        filename: 'static/css/[name].[contenthash].css',
+      }
+    ), new CompiledExtractPlugin())
+
+    config.optimization.minimizer.push(
+        new CssMinimizerPlugin(),
+    );
+
+    config.optimization.minimize = true;
 
     return config
   },
